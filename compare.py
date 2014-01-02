cabe8e0fdb0eb558a953b6798a01471898e845a3
evanl-laptop <evanl@princeton.edu>
2014-01-02 17:39:27 -0500

    added looping to compare

--- compare.py
+++ compare.py
@@ -10,33 +10,41 @@ import tough.t2.process_t2_output as pt2
 import vesa.VESA_02_01_13.vesa_reading_functions as vr
 
 class Compare:
-    def __init__(self, vesa_simtitle, tough_simtitle,\
+    def __init__(self, sim_titles, num_vesa_sims, 
             sleipner, section,\
             vesa_hydro_folder = 'unif_hydro'):
+        self.num_vesa_sims = num_vesa_sims
+        self.simulations = []
         print "------------------------------------------------------------\n"
-        print "Comparing "
-        print "  VESA simulation: " + vesa_simtitle
+        print "Comparing VESA Simulations:"
+        for i in range(num_vesa_sims):
+            print 20*" " + sim_titles[i]
         print "with"
-        print "TOUGH2 simulation: " + tough_simtitle
+        print "          TOUGH2 Simulations:"
+        for i in range(num_vesa_sims, len(sim_titles)):
+            print  20*" "+ tough_simtitle
         print "------------------------------------------------------------\n"
         current_directory = os.getcwd()
-        vesa_dirname =  current_directory + "/vesa/VESA_02_01_13/" + vesa_simtitle
-        tough_dirname = current_directory + "/tough/t2/" + tough_simtitle + "_dir/"
-
-        os.chdir(tough_dirname)
-        t2_grid, t2_timesteps = pt2.process_t2_output(tough_simtitle)
+        for i in range(num_vesa_sims):
+            print "reading VESA Simulation: " + sim_titles[i]
+            vesa_dirname =  current_directory + "/vesa/VESA_02_01_13/" + sim_titles[i]
+            os.chdir(vesa_dirname)
+            vesa_cells, vesa_timesteps = vr.read_output_data(layer = vesa_simtitle)
+            self.simulations.append(Simulation(sim_titles[i], 'vesa', \
+                    vesa_cells, vesa_timesteps))
+        for i in range(num_vesa_sims, len(sim_titles)):
+            print "reading TOUGH2 Simulation: " + sim_titles[i]
+            tough_dirname = current_directory + "/tough/t2/" + sim_titles[i] + "_dir/"
+            os.chdir(tough_dirname)
+            t2_grid, t2_timesteps = pt2.process_t2_output(tough_simtitle)
+            self.simulations.append(Simulation(sim_titles[i], 'tough', \
+                    t2_grid, t2_timesteps))
         # this reads the output for a given unit, but will not work for 
         # multiple units. the positions will have to be combined for that later
-        os.chdir(vesa_dirname)
-        vesa_cells, vesa_timesteps = vr.read_output_data(layer = vesa_simtitle)
 
         os.chdir(current_directory)
-        print "read both, switched back to the present directory"
+        print "YAY"
 
-        self.t2_grid = t2_grid
-        self.t2_timesteps = t2_timesteps
-        self.vesa_cells = vesa_cells
-        self.vesa_timesteps = vesa_timesteps
         self.sleipner = sleipner
         self.section = section
 
@@ -67,6 +75,7 @@ class Compare:
             CS = ax_c.contourf(x,y,z,v)
         else:
             CS = ax_c.contourf(x,y,z)
+        cbaxes = self.fig.add_axes([0.8,0.1,0.03,0.8])
         CB = plt.colorbar(CS, shrink = 1.0, pad=0.02, fraction = 0.07,\
                 extend = 'both', format='%.2f')
         #if label != False:
@@ -88,88 +97,135 @@ class Compare:
         ax_g.set_title(title)
         return 0
 
-    def add_t2_contours(self, axis, index, valtype):
+    def add_t2_contours(self, sim_index, axis, space_index, valtype,\
+            time_indices = [0,1]):
         pos = 220
-        for time_index in [1,5]:
-            self.t2_timesteps[time_index].make_plot_grid(self.t2_grid, \
-                    axis, index,\
+        for time_index in time_indices:
+            self.simulations[sim_index].time_steps[time_index].make_plot_grid(\
+                    self.simulations[sim_index].grid_cells, \
+                    axis, space_index,\
                     valtype)
-            x, y, z = self.t2_timesteps[time_index].format_plot_grid(\
-                    self.t2_grid, axis, self.sleipner, self.section)
+            x, y, z = self.simulations[sim_index].time_steps[time_index].format_plot_grid(\
+                    self.simulations[sim_index].grid_cells, \
+                    axis, self.sleipner, self.section)
             pos +=1
             label_str = self.year_index(time_index)
+            if axis == 3:
+                xlab = 'x [m]'
+                ylab = 'y [m]'
+            elif axis == 2:
+                xlab = 'y [m]'
+                ylab = 'z [m]'
+            elif axis == 1:
+                xlab = 'x [m]'
+                ylab = 'z [m]'
             self.plot_contour(x,y,z, position = pos, \
+                    xlab = xlab, ylab = ylab,\
                     title = label_str, label = 'saturation []')
         return 0
 
-    def add_vesa_sections(self, axis, index, nx):
+    def add_vesa_sections(self, sim_index, axis, space_index, nx,\
+            time_indices=[0,1]):
         pos = 222
-        for time_index in [1,5]:
+        for time_index in time_indices:
             pos +=1
-            ys, zb, zt, plume = vr.make_cross_sections(self.vesa_cells, \
-                    time_index, axis, index, nx)
+            ys, zb, zt, plume = vr.make_cross_sections(\
+                    self.simulations[sim_index].grid_cells, \
+                    time_index, axis, space_index, nx)
             title_string = self.year_index(time_index)
             self.plot_graph(ys, plume, pos)
             self.plot_graph(ys, zb, pos)
             self.plot_graph(ys, zt, pos, title = title_string)
         return 0
 
-    def add_vesa_contours(self, nx, ny, valtype):
-        x, y, zval = vr.make_plot_grid(self.cells, time_index, \
-                nx, ny, valtype)
-        self.plot_contour(x, y, zval, position = pos,\
-                label = valtype)
-
+    def add_vesa_contours(self, sim_index, nx, ny, valtype, \
+            time_indices=[0,1]):
+        pos = 222
+        for time_index in time_indices:
+            pos +=1
+            x, y, zval = vr.make_plot_grid(\
+                    self.simulations[sim_index].grid_cells, time_index, \
+                    nx, ny, valtype)
+            self.plot_contour(x, y, zval, position = pos,\
+                    xlab = 'x [m]', ylab = 'y [m]',\
+                    label = valtype)
         return 0
 
-    def create_cross_section_comparison(self, title):
+    def create_cross_section_comparison(self, title, time_indices=[0,1]):
         self.create_blank_figure(compare_type ='section')
-        fmt = 'eps'
+        fmt = 'png'
         nx = 65
         ny = 119
         x_ind = nx/2
         y_ind = ny/2
-        self.add_t2_contours(2, x_ind, 'saturation')
-        self.add_vesa_sections(2, x_ind, nx)
-        #self.fig.savefig('x_section_saturation.'+fmt, bbox_inches='tight',format=fmt)
+        v_index = 0
+        t_index = 1
+        self.add_t2_contours(t_index, 2, x_ind, 'saturation',\
+                time_indices = time_indices)
+        self.add_vesa_sections(v_index, 2, x_ind, nx,\
+                time_indices = time_indices)
+        self.fig.tight_layout()
         self.fig.savefig(title + '.' + fmt, bbox_inches='tight',format=fmt)
         self.fig.clf()
         return 0
+
+    def create_contour_comparison(self, title, time_indices=[0,1]):
+        nx = 65
+        ny = 119
+        z_ind = 3
+        self.create_blank_figure(fontsize = 10, compare_type = 'horizontal')
+        fmt = 'png'
+        valtype = 'saturation'
+        v_index = 0
+        t_index = 1
+        self.add_t2_contours(t_index, 3, z_ind, valtype,\
+                time_indices = time_indices)
+        self.add_vesa_contours(v_index,nx, ny, valtype,\
+                time_indices = time_indices)
+        self.fig.tight_layout()
+        self.fig.savefig(title + '.' + fmt, bbox_inches='tight', format=fmt)
+        self.fig.clf()
+        return 0
+
     def year_index(self, time_index):
         yrstring = '{:4d}'.format(int(1999 + time_index))
         return yrstring
+
+class Simulation:
+    def __init__(self, sim_title, sim_type, grid_cells, time_steps):
+        self.sim_title = sim_title
+        self.sim_type = sim_type
+        self.grid_cells = grid_cells
+        self.time_steps = time_steps
 
 
 if __name__ == '__main__': 
     if len(sys.argv) != 3:
         sys.exit("Proper usage for compare.py is \n" +
                 "python compare.py <vesa_simtitle> <tough_simtitle>\n")
+    sim_titles = []
+    for i in range(1,len(sys.argv)):
+        sim_titles.append(sys.argv[i])
     vesa_simtitle = sys.argv[1]
     tough_simtitle = sys.argv[2]
+    num_vesa_sims = 1
     sleipner = True
     section = False
-    c = Compare(vesa_simtitle, tough_simtitle, sleipner, section)
-
-    cl = [[1.,2.,3.],[4.,5.,6.],[7.,8.,9.]]
-    z = np.asarray(cl)
-    z = z / z.max()
-    xl = [1.,2.,3.]
-    yl = [4.,5.,6.]
-    yl1 = [4.5, 5., 5.5]
-    x, y = np.meshgrid(xl, yl)
-    xg = np.asarray(xl)
-    yg = np.asarray(yl1)
-
-    title = 'sleipner_comparison_1'
-    c.create_cross_section_comparison(title)
-    c.create_blank_figure(fontsize = 4, compare_type = 'horizontal')
-    pos = 230
-    for i in range(3):
-        pos += 1
-        c.plot_contour(x, y, z, position = pos)
-    for i in range(3):
-        pos +=1
-        c.plot_contour(x, y, z, position = pos)
-    fmt = 'eps'
-    c.fig.savefig('testhoriz.eps',bbox_inches='tight',format=fmt)
-    c.fig.clf()
+    c = Compare( sim_titles, num_vesa_sims, sleipner, section)
+    temp = '32'
+    yr_index = [0,2,4,6,8]
+    for i in yr_index:
+        time_indices = [i, i+1]
+        if i == 0:
+            yri = 99
+        else:
+            yri = i-1
+        title = 'sleipner_' + temp + '_section_' + str(yr_index.index(i)) + \
+                '_' + '{:2d}'.format(yri) + '_' + \
+                '{:2d}'.format(i)
+        c.create_cross_section_comparison(title, time_indices = time_indices)
+        contour_title = 'sleipner_' + temp + '_contour_' +\
+                str(yr_index.index(i)) + \
+                '_' + '{:2d}'.format(i-1) + '_' + \
+                '{:2d}'.format(i)
+        c.create_contour_comparison(contour_title, time_indices = time_indices)
